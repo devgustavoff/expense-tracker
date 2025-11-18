74c396a72955f537bfd36f9cf78471284cf21bd0
Gustavo Fernandes <gustavofernandes.f@outlook.com>
2025-11-18 09:37:45 -0300

    Codigo refatorado

--- expense-tracker.py
+++ expense-tracker.py
@@ -1,14 +1,14 @@
 import argparse
 import sqlite3
 from datetime import datetime
-import pandas as pd
 import calendar
 
 # Classe que representa as expenses
 class Expense:
-    def __init__(self, description, amount):
+    def __init__(self, description, amount, category):
         self.description = description
         self.amount = amount
+        self.category = category
         self.date = datetime.now()
 
 # Constante que armazena uma string que representa o nome do banco de dados
@@ -29,7 +29,8 @@ with sqlite3.connect(EXPENSE_FILE) as connection:
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         description TEXT NOT NULL,
         amount REAL,
-        date TEXT
+        date TEXT,
+        category TEXT
     );
     """
     # Aqui estamos executando o metodo execute() para executar a query dentro do DB o arquivo
@@ -59,15 +60,15 @@ def insertion_expense(expense):
 
         # Query para inserir dados no DB
         insert_query = """
-        INSERT INTO Expenses(description, amount, date)
-        VALUES (?, ?, ?);
+        INSERT INTO Expenses(description, amount, date, category)
+        VALUES (?, ?, ?, ?);
         """
         # Aqui é obtido a data que a expense foi inserida na tabela
         # E também estamos formatando ela para o formato ISO
         current_datatime = expense.date.isoformat()
 
         # Aqui temos uma tupla com os dados da expenses
-        expense_data = (expense.description, expense.amount, current_datatime)
+        expense_data = (expense.description, expense.amount, current_datatime, expense.category)
 
         # Executamos a query de inserção junto com os dados da expense que sera iserida no DB
         cursor.execute(insert_query, expense_data)
@@ -77,45 +78,68 @@ def insertion_expense(expense):
 
         print(f"Expense added successfully")
 
+def get_all_expenses():
+    """Obtains all expenses on the DB.
 
-# Essa função mostra todas as expenses no DB
-def list_expenses():
+    :return: list - list of dictionary containing expense data 
+    """
+    with sqlite3.connect(EXPENSE_FILE) as connection:
+        cursor = connection.cursor()
+        select_query = """
+        SELECT id, date, description, amount, category
+        FROM Expenses;
+        """
+        cursor.execute(select_query)
+        column_names = [desc[0] for desc in cursor.description]
+        rows = cursor.fetchall()
+        return [dict(zip(column_names, row)) for row in rows]
+
+def list_expenses(category):
     """Display all expenses in formatted table
 
     :return: None
     """
+    expenses = get_all_expenses()
     
-    # Instrução que faz a conecxão com o DB
+    if not expenses:
+        id_width = len('ID')
+        date_width = len('Date')
+        desc_width = len('Description')
+        amount_width = len('Amount')
+        ctg_width = len('Category')
+    else:
+        if category:
+            expenses = [expense for expense in expenses if expense['category'] == category]
+        
+        id_width = max(len('ID'), max(len(str(exp['id'])) for exp in expenses))
+        date_width = max(len('Date'), max(len(exp['date']) for exp in expenses))
+        desc_width = max(len('Description'), max(len(exp['description']) for exp in expenses))
+        amount_width = max(len('Amount'), max(len(f"${exp['amount']:.2f}") for exp in expenses))
+        ctg_width = max(len('Category'), max(len(exp['category']) for exp in expenses))
+
+    
+    print(f"{'ID':<{id_width}} {'Date':<{date_width}} {'Description':<{desc_width}} {'Amount':<{amount_width}} {'Category':<{ctg_width}}")
+    for expense in expenses:
+        print(f"{expense['id']:<{id_width}} {expense['date']:<{date_width}} {expense['description']:<{desc_width}} {f'${expense['amount']:.2f}':<{amount_width}} {expense['category']:<{ctg_width}}")
+
+def get_expense_by_id(expense_id):
+    """Obtains a specific expense
+
+    :param expense_id: int  - ID of expense
+    :return: dict or None - expense data if found, None otherwise
+    """
+
     with sqlite3.connect(EXPENSE_FILE) as connection:
-
-        # Query que seleciona e motra as colunas expecificadas de Expenses
-        select_query = "SELECT id, date, description, amount FROM Expenses;"
-
-        # Aqui usamos a biblioteca pandas para criar um dataframe
-        df = pd.read_sql_query(select_query, connection)
-
-        # Instrução que renomeia o nome das colunas
-        df = df.rename(columns={
-            'id':'ID',
-            'date':'Date',
-            'description':'Description',
-            'amount':'Amount'
-        })
-
-        # Converte a data para o formato YYYY/MM/DD
-        df['Date'] = pd.to_datetime(df['Date']).dt.date
-
-        # Esse instrunção transforma valores Null/NaN no formato 0.00
-        df['Amount'] = df['Amount'].fillna(0.0)
-
-        # Essa instrução aplica a função lambda em todos os valores da coluna amount
-        # A expressão que a lambda recebe de conversão, essa expressão converte todo valor x em um valor com 2 casas decimais
-        df['Amount'] = df['Amount'].apply(lambda x: f"${x:.2f}")
-
-        # Imprime o dataframe no formato string, o index=Fales é para desativar os indices das linhas
-        # não quero mostrar os idices das linhas
-        print(df.to_string(index=False))
-
+        cursor = connection.cursor()
+        select_query = """
+        SELECT id, date, description, amount
+        FROM Expenses
+        WHERE id = ?
+        """
+        cursor.execute(select_query, (expense_id,))
+        columns = [desc[0] for desc in cursor.description]
+        data = cursor.fetchone()
+        return dict(zip(columns, data)) if data else None
 
 def update_expense(expense_id, amount):
     """Update an expense existente
@@ -123,21 +147,20 @@ def update_expense(expense_id, amount):
     :param expense_id: int - id of expense.
     :param amount: float -  value of expense.
     """
-
-    with sqlite3.connect(EXPENSE_FILE) as connection:
-
-        cursor = connection.cursor()
-
-        update_query = """
-        UPDATE Expenses
-        SET amount = ?
-        WHERE id = ?;
-        """
-        cursor.execute(update_query, (amount, expense_id))
-
-        connection.commit()
-
-        print(f"Expense updated successfully (ID: {expense_id})")
+    
+    expense = get_expense_by_id(expense_id)
+    if expense:
+        with sqlite3.connect(EXPENSE_FILE) as connection:
+            cursor = connection.cursor()
+            update_query = """
+            UPDATE Expenses
+            SET amount = ?
+            WHERE id = ?;
+            """
+            cursor.execute(update_query, (amount, expense_id))
+            print(f"Expense updated successfully")
+    else:
+        print("Invalid ID")
 
 def delete_expense(expense_id):
     """Delete an specific expense
@@ -146,34 +169,55 @@ def delete_expense(expense_id):
     :return: None
     """
 
-    with sqlite3.connect(EXPENSE_FILE) as connection:
-
-        cursor = connection.cursor()
-
-        select_query = """
-        SELECT 1
-        FROM Expenses
-        WHERE id = ?
-        """
-
-        cursor.execute(select_query, (expense_id,))
-
-        aux = cursor.fetchone()
-
-        if aux != None:
-
+    expense = get_expense_by_id(expense_id)
+    if expense:
+        with sqlite3.connect(EXPENSE_FILE) as connection:
+            cursor = connection.cursor()
             delete_query = """
-            DELETE FROM Expenses
-            WHERE id = ?;
+            DELETE
+            FROM Expenses 
+            WHERE id = ?
             """
             cursor.execute(delete_query, (expense_id,))
+            print("Expense deleted successfully")
+    else:
+        print("Invalid ID")
 
-            connection.commit()
+def get_total_expenses():
+    """Sum the total amount of all expenses
 
-            print(f"Expense deleted successfully (ID: {expense_id})")
-        else:
-            print("Expense not found")
+    :return: float - the total of colum amount
+    """
 
+    with sqlite3.connect(EXPENSE_FILE) as connection:
+        cursor = connection.cursor()
+        sum_query = """
+        SELECT SUM(amount)
+        FROM Expenses
+        """
+        cursor.execute(sum_query)
+        result = cursor.fetchone()
+        return result[0] if result[0] is not None else 0.0
+
+def get_month_total(month):
+    """Sum the total of all expenses of specific month
+
+    :param month: string - number of month
+    :return: float - total of expenses of specific month
+    """
+    
+    month = str(month).zfill(2)
+    
+    with sqlite3.connect(EXPENSE_FILE) as connection:
+        cursor = connection.cursor()
+        sum_query = """
+        SELECT SUM(amount)
+        FROM Expenses
+        WHERE strftime('%m', date) = ?
+        """
+        cursor.execute(sum_query, (month,))
+        result = cursor.fetchone()
+        return result[0] if result[0] is not None else 0.0
 
 def summary_total():
     """Display summary total of expenses
@@ -181,47 +225,17 @@ def summary_total():
     :return: None
     """
 
-    with sqlite3.connect(EXPENSE_FILE) as connection:
-
-        cursor = connection.cursor()
-
-        summary_total_query = """
-        SELECT SUM(amount)
-        FROM Expenses
-        """
-
-        cursor.execute(summary_total_query)
-
-        total_expenses = cursor.fetchone()
-
-        print(f"Total expenses: ${total_expenses[0]:.2f}")
+    print(f"Total expenses: ${get_total_expenses():.2f}")
 
 def summary_month(month):
     """Display summary of a specific month
 
     :return: None
     """
-    
-    with sqlite3.connect(EXPENSE_FILE) as connection:
-
-        cursor = connection.cursor()
-
-        summary_month_query = """
-        SELECT SUM(amount)
-        FROM Expenses
-        WHERE strftime('%m', date) = ?
-        """
-
-        cursor.execute(summary_month_query, (month,))
-
-        month_expenses = cursor.fetchone()
-
-        name_of_month = calendar.month_name[int(month)]
-
-        try:
-            print(f"Total expenses for {name_of_month} ${month_expenses[0]:.2f}")
-        except TypeError:
-            print(f"Date not found")
+    try:
+        print(f"Total expenses for {calendar.month_name[int(month)]}: ${get_month_total(month):.2f}")
+    except (IndexError, ValueError):
+        print("Invalid month")
 
 # Instanciamos um objeto analisador sintatico 
 parser = argparse.ArgumentParser(description='Manage expenses')
@@ -238,8 +252,12 @@ parser_add.add_argument('--description', action='store', help='Description of ex
 # Argumento opcional para adcionar o valor da despesa
 parser_add.add_argument('--amount', type=float, action='store', help='Amount of expense')
 
+parser_add.add_argument('--category', type=str, action='store', help='Category of expense')
+
 # Comando para listar todas as despesas
 parser_list = subparsers.add_parser('list', help='List all expenses in lista of expenses')
+
+parser_list.add_argument('--category', type=str, action='store', help='Name of category')
 
 # Comando para atualizar uma despesa especifica
 parser_update = subparsers.add_parser('update', help='Update the amount of expense')
@@ -267,13 +285,13 @@ parser_summary.add_argument('--month', type=str, action='store', help="Show the 
 args = parser.parse_args()
 
 if args.command == 'add':
-    insertion_expense(Expense(description=args.description, amount=args.amount))
+    insertion_expense(Expense(description=args.description, amount=args.amount, category=args.category))
 elif args.command == 'update':
     update_expense(args.id, args.amount)
 elif args.command == 'delete':
     delete_expense(args.id)
 elif args.command == 'list':
-    list_expenses()
+    list_expenses(category=args.category)
 elif args.command == 'summary':
     if args.month:
         summary_month(args.month)
